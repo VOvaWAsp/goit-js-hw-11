d44d914d694ac1a52bee85dadb09b45ea6ae17f4
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-12-11 11:47:07 +0100

    cat

--- src/searchForm.js
+++ src/searchForm.js
@@ -6,78 +6,91 @@ const gallery = document.querySelector(".gallery");
 const loadMore = document.querySelector(".load-more");
 let page = 1;
 loadMore.style.display = "none"
+// loadMore.style.display = "none"
 
 searchForm.addEventListener("submit", handleSubmit)
 
 function handleSubmit(event) {
 event.preventDefault()
+page = 1;
 
 const { searchQuery } = event.currentTarget.elements;
-const obj = {
-    searchFormyValue: searchQuery.value
-}
-
-    function handleClick() {
-
-        page += 1;
-    
-        searchingSystem(page)
-        .then(data => {
-            gallery.insertAdjacentHTML("beforeend", createMarkup(data.data.hits))
-            // console.log(data.data.hits)
-            if (page === 26) {
-                loadMore.style.display = "none"
-            }
-            // console.log(data.data.hits.length)
-        })
-        .catch(error => console.log(error))
-    }
-
-async function search() {
-   try {        
-    const data = await searchingSystem();
-    if (data.data.totalHits === 0) {
-        Notiflix.Notify.failure('Qui timide rogat docet negare');
-        loadMore.style.display = "none"
-    } else {
-        loadMore.style.display = "block"
-        Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
-    }
-    // console.log(data.data)
-    gallery.innerHTML = createMarkup(data.data.hits)
-    //    console.log(createMarkup(data.hits))
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-    async function searchingSystem(page = 1) {
-        const BASE_URL = "https://pixabay.com/api/";
-        const key = "41168195-d63dcd7c5ed901c12bfe9d8da";
-        const SEARCH = obj.searchFormyValue;
-    
-        const params = new URLSearchParams({
-            key,
-            q: SEARCH,
-            image_type: "photo",
-            orientation: "horizontal",
-            safesearch: true,
-            page: page,
-        })
-
-        try {
-            const results = await axios.get(`${BASE_URL}?${params}`);
-            return results
-            // return console.log(results.data)
-        } catch(error) {
-            console.log(error);
-        }
-}
+searchValue = searchQuery.value;
 loadMore.addEventListener("click", handleClick);
 search()
-searchingSystem()
-
+// searchingSystem()
+// const obj = {
+//     searchFormyValue: searchQuery.value
+// }
 }
+
+function handleClick() {
+    // loadMore.style.display = "none"
+    page += 1;
+
+    searchingSystem(page)
+    .then(data => {
+        gallery.insertAdjacentHTML("beforeend", createMarkup(data.data.hits))
+        // console.log(data.data.hits)
+        // if (page === 1) {
+        //         loadMore.style.visibility = "hidden"
+        // }
+        // console.log(data.data.hits)
+    })
+    .catch(error => console.log(error))
+}
+
+async function search() {
+try {        
+const data = await searchingSystem();
+if (data.data.totalHits === 0) {
+    Notiflix.Notify.failure('Qui timide rogat docet negare');
+    loadMore.style.display = "none"
+} else {
+    loadMore.style.display = "block"
+    Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
+}
+// console.log(data.data)
+gallery.innerHTML = createMarkup(data.data.hits)
+//    console.log(createMarkup(data.hits))
+} catch (error) {
+    console.log(error);
+}
+};
+
+async function searchingSystem(page = 1) {
+    const BASE_URL = "https://pixabay.com/api/";
+    const key = "41168195-d63dcd7c5ed901c12bfe9d8da";
+    const q = searchValue;
+
+    // const params = new URLSearchParams({
+    //     key,
+    //     q: SEARCH,
+    //     image_type: "photo",
+    //     orientation: "horizontal",
+    //     safesearch: true,
+    //     page: page,
+    // })
+
+    try {
+        const results = await axios.get(`${BASE_URL}`, {
+            params: {
+                key,
+                q,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true,
+                page: page,
+            }
+        });
+        // loadMore.style.display = "none"
+        return results
+        // return console.log(results.data)
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 function createMarkup(arr) {
     return arr.map(({webformatURL, tags, likes, views, comments, downloads}) => `
     <div class="photo-card">
