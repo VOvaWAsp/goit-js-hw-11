208cb22e4ab24c74421ec4a5d698f43e04dd9d9a
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-12-12 20:37:59 +0100

    donees

--- src/searchForm.js
+++ src/searchForm.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-Described in documentation
+// Described in documentation
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
